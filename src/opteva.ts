0837ce39fdc0d39493988f8c83e3aee736bb65f2
Zim <zimtsui@gmail.com>
2026-03-15 15:07:37 +0800

    optiva

---
+++ src/opteva.ts
@@ -0,0 +1,44 @@
+export class Opposition {
+    public constructor(public message: string) {}
+}
+export class Rejection {
+    public constructor(public message: string) {}
+}
+
+
+/**
+ * @throws outgoing {@link Opposition}
+ * @throws incoming {@link Rejection}
+ */
+export interface Optimize extends AsyncGenerator<string, never, void> {}
+
+/**
+ * @throws outgoing {@link Rejection}
+ * @throws incoming {@link Opposition}
+ */
+export interface Evaluate extends AsyncGenerator<void, never, string> {}
+
+
+/**
+ * @throws {@link Rejection}
+ */
+export async function opteva(opt: Optimize, eva: Evaluate): Promise<void> {
+    const draft = await opt.next().then(r => r.value);;
+    try {
+        return await eva.next(draft).then(r => r.value);
+    } catch (e) {
+        if (e instanceof Rejection) {} else throw e;
+        for (let rejection = e;;) try {
+            await opt.throw(rejection);
+            throw rejection;
+        } catch (e) {
+            if (e instanceof Opposition) {} else throw e;
+            try {
+                return await eva.throw(e).then(r => r.value);
+            } catch (e) {
+                if (e instanceof Rejection) {} else throw e;
+                rejection = e;
+            }
+        }
+    }
+}
